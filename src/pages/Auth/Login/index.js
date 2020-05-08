fed4f8966c27727ad4347637226997c1acfddb2b
dev-jefferson <jhsg.dev@gmail.com>
2020-05-08 20:31:05 -0300

    elaborado pagina de login e os estilos

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { FiLogIn } from "react-icons/fi";
 
 import './styles.css';
 
+import logoImg from "../../../assets/logo.svg";
+import heroesImg from "../../../assets/heroes.png";
+
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero"/>
+
+        <form action="">
+          <h1>Faça seu Login</h1>
+
+          <input placeholder="Seu ID"/>
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size="16" color="#E02041"/>
+            Não tenho cadastro
+            </a>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes"/>
+    </div>
+  );
 }
 
