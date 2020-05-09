5ebf71581dd50a36eaf71e2f4def167bbcd4248a
dev-jefferson <jhsg.dev@gmail.com>
2020-05-08 23:45:53 -0300

    iniciando a parte do registro

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 // import { Container } from './styles';
 export default function Routes() {
@@ -9,7 +10,8 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Login}/>
+        <Route path="/" exact component={Login}/>
+        <Route path="/register" component={Register}/>
       </Switch>
     </BrowserRouter>
   );
