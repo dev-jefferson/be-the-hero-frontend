4525430ce3b32a325d8382b06bb8771fc945bd76
dev-jefferson <jhsg.dev@gmail.com>
2020-05-09 02:16:56 -0300

    feito tela profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 import './styles.css';
@@ -20,6 +20,43 @@ function Profile() {
           <FiPower size="18" color="#E02041"/>
         </button>
       </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong> <p>Caso Teste</p>
+          <strong>DESCRIÇÃO:</strong> <p>Descrição de Teste</p>
+          <strong>VALOR:</strong> <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size="20" color="#a8a8b3"/>
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong> <p>Caso Teste</p>
+          <strong>DESCRIÇÃO:</strong> <p>Descrição de Teste</p>
+          <strong>VALOR:</strong> <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size="20" color="#a8a8b3"/>
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong> <p>Caso Teste</p>
+          <strong>DESCRIÇÃO:</strong> <p>Descrição de Teste</p>
+          <strong>VALOR:</strong> <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size="20" color="#a8a8b3"/>
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong> <p>Caso Teste</p>
+          <strong>DESCRIÇÃO:</strong> <p>Descrição de Teste</p>
+          <strong>VALOR:</strong> <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size="20" color="#a8a8b3"/>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
